e358c8f4beea2db8a3e85730e79249c696b2e57e
Eduardo Lerma <lalo_pichu22@hotmail.com>
2020-07-14 18:47:13 -0500

    Started part2 exercises

---
+++ part2/courseinfo/src/components/course.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Course = ({course}) => {
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    course.parts.map(part =>
+      <Part key={part.name} part={part}/>
+    )
+  )
+}
+
+export default Course
