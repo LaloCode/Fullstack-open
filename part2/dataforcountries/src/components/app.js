0953758549cc53feca2f771023b898f594f40a4b
Eduardo Lerma <lalo_pichu22@hotmail.com>
2020-07-23 23:14:27 -0500

    started base work on dataforcountries

--- part2/dataforcountries/src/components/app.js
+++ part2/dataforcountries/src/components/app.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import FindCountries from './findCountries'
 
 const App = () => {
     return (
-        <div>start</div>
+        <div>
+            <FindCountries />
+        </div>
     )
 }
 
---
+++ part2/dataforcountries/src/components/findCountries.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FindCountries = (props) => (
+    <div>
+        find countries <input />
+    </div>
+)
+
+export default FindCountries
